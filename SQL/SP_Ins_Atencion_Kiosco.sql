b5ece84508bdc0485dc28ff95236445792a26415
DeveloperJ11 <jearson.developer11@gmail.com>
2017-01-13 17:02:55 -0600

    Funcionando

--- SQL/SP_Ins_Atencion_Kiosco.sql
+++ SQL/SP_Ins_Atencion_Kiosco.sql
@@ -1,12 +1,12 @@
 DROP PROCEDURE IF EXISTS Inserta_Atencion_Kiosco;
 DELIMITER //
-CREATE PROCEDURE Inserta_Atencion_Kiosco(Kiosco_ID VARCHAR(15), Empleado_ID INT)
+CREATE PROCEDURE Inserta_Atencion_Kiosco(Fecha DATETIME, Kiosco_ID VARCHAR(15), Empleado_ID INT)
 BEGIN
 
 	INSERT INTO copycat.atencion_kiosco
 				(Fecha_Atencion_Kiosco, Kiosco_ID_Kiosco, Empleado_ID_Empleado)
 	VALUES
-				(NOW(),Kiosco_ID,Empleado_ID);
+				(Fecha,Kiosco_ID,Empleado_ID);
 
 END//
 DELIMITER ;
