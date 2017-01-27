edefefe146d364a45bed826e8cadcc1fdca64021
DeveloperJ11 <jearson.developer11@gmail.com>
2017-01-27 12:27:25 -0600

    27 Enero Final

--- Compra_Clientes.php
+++ Compra_Clientes.php
@@ -6,7 +6,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta http-equiv="Content-Type" content="text/html;" charset="iso-8859-1" />
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
 <title>CopyCat</title>
@@ -34,7 +34,7 @@
 	function GetDataCliente(){		
 		var obj = document.getElementById('txt_Nit_Cliente');
 		loadXMLDoc('GetCliente.php?NiC=' + obj);
-		alert (obj);
+		alert(obj);
 	}
 
 </script>
